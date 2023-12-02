6d5a8346a2c049988414b5f41a8dcfdadd5a4fdf
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2023-12-02 14:37:29 +0600

    more nensted

--- X-ploit/python/Scoping Visualization.py
+++ X-ploit/python/Scoping Visualization.py
@@ -3,8 +3,12 @@ a = "Global Stuffs Earth"
 def Bangladesh_f():
     a = "Bangladesh"#Level 1 local area
     def Khulna_f():
-        nonlocal a # nonlocal means inside a = "Khulna"
+        nonlocal a # nonlocal means inside "Khulna" everyhing
         a = "Khulna"#Level 2 local area
+        def KCC_f():
+            nonlocal a # nonlocal means inside "KCC" everyhing
+            a = "KCC Royal more"#Level 3 local area
+            print(a)
         print(a)
     Khulna_f()
     print("Inside Bangladesh", a)
