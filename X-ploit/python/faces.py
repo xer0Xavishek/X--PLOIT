bdd39a5c27f79e84efe0bb4e86e0b11d7eef3eae
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2023-12-02 14:13:39 +0600

    scoping

--- X-ploit/python/faces.py
+++ X-ploit/python/faces.py
@@ -1,10 +1,13 @@
-def main():
-    s = input()
-    T=convert(s)
-    print(T)
+a = "Global a"
 
-def convert(s):
-    s = s.replace(":)","🙂").replace(":(","🙁")
-    return s
+def outer_f():
+    a = "Outer a(local)"
+    def inner_f():
+        nonlocal a
+        a = "Inner a(nonlocal)"
+        print("Inside inner_f(): ", a)
+    inner_f()
+    print("Inside outer_f(): ", a)
 
-main()
+outer_f()
+print("In the main: ", a)
