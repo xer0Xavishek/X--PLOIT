e708cfcb26d12555cb5c20a0073f0de887b9f90d
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2024-01-12 10:59:22 +0600

    ff

--- X-ploit/Non_Academic/test.py
+++ X-ploit/Non_Academic/test.py
@@ -6,7 +6,8 @@ class Human:
 usr1=Human()
 usr2=Human()
 
+usr1.age=20     #method 1
+usr2.age=30     #method 2
 
-
-usr1.print_age()
-usr2.print_age()
+usr1.print_age("Jamal")
+usr2.print_age("Kamal")
