4d529838ebfdeaabec5808d09f70f9fa9f85bed6
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2024-01-12 10:49:03 +0600

    oishi pagol

--- X-ploit/Non_Academic/test.py
+++ X-ploit/Non_Academic/test.py
@@ -1,10 +1,12 @@
 class Human:
     age=0
-    def print_age(self):
-        print(self.age)
+    def print_age(self,additional_text):
+        print(self.age,additional_text)
 
 usr1=Human()
 usr2=Human()
-usr1.age=20
-usr2.age=30
+
+
+
 usr1.print_age()
+usr2.print_age()
